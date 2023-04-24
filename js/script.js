c653cc872d7cf965deb6029e301da71ed70c5e2a
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-24 09:15:11 -0300

    Create Function Get Repos e append li na View

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const ul = document.querySelector('ul')
+
+function getApiGithub() {
+    // Busca na api do github de Repositorios
+    fetch('https://api.github.com/users/omartins-zs/repos')
+        .then(async res => {
+            // Verifica a requisição se a API existe
+            if (!res.ok) {
+                throw new Error(res.status)
+            }
+
+            var data = await res.json()
+
+            data.map(item => {
+                let li = document.createElement('li')
+
+                // Escrevendo a estrutura da Li
+                li.innerHTML = `
+                <strong>${item.name.toUpperCase()}</strong>
+                <span>${item.url}</span>
+                <span>Data Criação: ${Intl.DateTimeFormat('pt-BR').format(new Date(item.created_at))}</span>
+                `
+
+                // Adicionando a li dentro do UL
+                ul.appendChild(li)
+            })
+
+        }).catch(e => console.log(e))
+}
+
+getApiGithub()
